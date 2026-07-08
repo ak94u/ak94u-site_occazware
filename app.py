ed06059839f495b64cd88caff8ad9e01d79e3f4f
ak94u <akrambenabdallah94@gmail.com>
2026-07-08 10:30:09 +0200

    changement du app.py pour mettre la route dans le dossier controllers

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import os
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 
-# Charger les variables du fichier .env
+# Importer le blueprint depuis le dossier controllers
+from controllers.accueil import page_accueil
+
 load_dotenv()
 
 app = Flask(__name__)
 
-# --- CONFIGURATION SÉCURISÉE DE LA BD ---
+# --- CONFIGURATION (Inchangée) ---
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
@@ -16,15 +18,13 @@ DB_NAME = os.getenv('DB_NAME')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Récupération de la clé secrète
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# --- ENREGISTREMENT DES ROUTEURS (BLUEPRINTS) ---
+# On dit à Flask d'activer les routes définies dans le controller
+app.register_blueprint(page_accueil)
 
 if __name__ == '__main__':
     app.run(debug=True)
